a2c11bf47db7951d9cd958560e4ade140c39fd05
Youssef IZIKITNE <youssef@localhost.localdomain>
2016-11-07 21:04:55 +0000

    More problems!

---
+++ acm.py
@@ -0,0 +1,15 @@
+tab = []
+x = input().split()
+while x[0] != '-1':
+	tab.append(x)
+	x = input().split()
+n = 0
+t = 0
+for i in tab :
+	if i[2] == 'right' :
+		n += 1
+		h = i[1]
+		t += int(i[0])
+		for j in tab:	
+			if j[1] == h and j[2] == 'wrong' : t += 20
+print(n, t)
---
+++ cups.py
@@ -0,0 +1,14 @@
+from operator import itemgetter as ig
+n = int(input())
+tab = []
+for i in range(n) :
+	tab.append(input().split())
+for i in range(n) :
+	try: 
+		x = int(tab[i][0])
+		tab[i][0], tab[i][1] = tab[i][1], int(int(tab[i][0])/2)
+	except :
+		tab[i][1] = int(tab[i][1])
+tab = sorted(tab, key = ig(int(1)))
+for i in tab :
+	print(i[0])
